f7e32531b56105c98a602d8e14df55122f251f7e
JohannBro <55952915+JohannBro@users.noreply.github.com>
2020-05-20 13:50:06 +0200

    Gestion de la batterie

--- Open_Weather_Map/Open_Weather_Map.ino
+++ Open_Weather_Map/Open_Weather_Map.ino
@@ -2,10 +2,9 @@
 * Meteo en direct via l'API d'Open Wearher Map sur WIFI KIT 32 de Heltec
 * Auteur : Johann Brochier
 * Brochier.IO
-* V0.3 2020
+* V0.4 2020
 */
  
-
 #include <ArduinoJson.h>
 #include <WiFi.h>
 #include "esp_wps.h"
@@ -18,14 +17,18 @@
 #define ESP_MODEL_NUMBER  "ESP32"
 #define ESP_MODEL_NAME    "ESPRESSIF IOT"
 #define ESP_DEVICE_NAME   "ESP STATION"
+#define Fbattery    3700  //Une batterie à 100% fait 3700mv
 
 static esp_wps_config_t config;
 
-const char* ssid = "......";  // SSID du Wifi
-const char* password =  "......"; // Mot de passe du Wifi
+const char* ssid = "......";  // SSID Wifi
+const char* password =  "......"; // Mot de passe Wifi
  
 const String endpoint = "https://api.openweathermap.org/data/2.5/weather?q=......,..&units=metric&lang=fr&appid="; // URL vers l'API OWM
 const String key = "......"; // Cle API de OWM
+
+int zeroBattery = 1930; // seuil bas, batterie à 2,6v
+double averageBattery = 100; // energie moyenne de la batterie initialisée à 100 pour le premier démarrage
 
 int cycle = 0; // Init du cycle pour la mise en veille
 
@@ -54,7 +57,7 @@ void WiFiEvent(WiFiEvent_t event, system_event_info_t info){
       break;
     case SYSTEM_EVENT_STA_GOT_IP:
       Serial.println("Connecté à :" + String(WiFi.SSID()));
-      Serial.print("Got IP: ");
+      Serial.print("IP: ");
       Serial.println(WiFi.localIP());
       break;
     case SYSTEM_EVENT_STA_DISCONNECTED:
@@ -130,8 +133,7 @@ void wifiConnect() {
 void wifiConnectWPS () {
   Heltec.display -> clear();
   WiFi.onEvent(WiFiEvent);
-  WiFi.mode(WIFI_MODE_STA);
-  
+  WiFi.mode(WIFI_MODE_STA);  
   Heltec.display->setTextAlignment(TEXT_ALIGN_CENTER);
   Heltec.display->setFont(ArialMT_Plain_16);
   Heltec.display->drawString(64,11,"Appuyez sur");
@@ -139,57 +141,12 @@ void wifiConnectWPS () {
   Heltec.display->drawString(64,43,"de votre routeur...");
   Heltec.display->display();
   Serial.println("Démarrage de WPS");
-  
   wpsInitConfig();
   esp_wifi_wps_enable(&config);
-  esp_wifi_wps_start(0);
-  
+  esp_wifi_wps_start(0);  
 }
 
 void displayWeather(String payload){
-  /* Decomposition de la chaine JSON reçue par l'API OWM
-         * {
-             "coord": {
-                "lon": -4.55,
-                "lat": 48.32
-              },
-              "weather": [{
-                "id": 803,
-                "main": "Clouds",
-                "description": "nuageux",
-                "icon": "04d"
-              }],
-              "base": "stations",
-              "main": {
-                "temp": 15.93,
-                "feels_like": 15.65,
-                "temp_min": 15,
-                "temp_max": 17,
-                "pressure": 1010,
-                "humidity": 87
-              },
-              "visibility": 10000,
-              "wind": {
-                "speed": 2.1,
-                "deg": 80
-              },
-              "clouds": {
-                "all": 81
-              },
-              "dt": 1589011313,
-              "sys": {
-                "type": 1,
-                "id": 6563,
-                "country": "FR",
-                "sunrise": 1588999569,
-                "sunset": 1589053389
-              },
-              "timezone": 7200,
-              "id": 2982811,
-              "name": "Roscanvel",
-              "cod": 200
-            }
-         */
         Heltec.display -> clear(); // on efface l'affichage
         StaticJsonDocument<2048> doc; 
         auto error = deserializeJson(doc, payload); // debut du parsing JSON
@@ -226,10 +183,8 @@ void displayWeather(String payload){
 }
 
 void displayIcon(String sky) {
-
   String sk = sky.substring(0,2);
   const char* icon;
-
   switch (sk.toInt()) {
     case 1:
       icon = one_bits;
@@ -288,7 +243,26 @@ void signalBars () {
 
 void batteryPower () {
   Heltec.display->drawRect(110,56,18,8);
-  // TODO Ajouter la gestion de la batterie
+  Heltec.display->fillRect(110,56,0.18*averageBattery,8);
+}
+double averagePower() {
+  double total = 0;
+  for (int i=0;i<=11;i++) {
+    uint16_t lec  =  analogRead(37);
+    Serial.println("Lecture : " + (String)lec);
+    double delta = (lec-zeroBattery);
+    Serial.println("delta : " + (String)delta);
+    double percent = (delta/810)*100;
+    if ( percent >= 100 ) {
+      percent = 100;
+    }
+    Serial.println("pourcentage : " + (String)percent);
+    total=total+percent;
+    delay(2000);
+  }
+  double averagePower = total/12;
+  Serial.println("averagePower : " + (String)averagePower);
+  return averagePower;
 }
 
 void lightSleep () {
@@ -302,52 +276,55 @@ void lightSleep () {
 }
 
 void setup() {
-  Heltec.begin(true /*DisplayEnable Enable*/, false /*LoRa Enable*/, true /*Serial Enable*/);  // Activalion de l'écran OLED
+  Heltec.begin(true /*DisplayEnable Enable*/, false /*LoRa Enable*/, true /*Serial Enable*/);  // Activalion de l'écran OLED et de la liaison série
   logo();  // Affiche le "splash screen"
   delay(2000);
   Heltec.display->clear();
-  
+  adcAttachPin(37); // Lecture du niveau de la batterie sur le GPIO37
+  analogSetClockDiv(255); // 1338mS 
   Serial.begin(115200);
-  
-  //wifiConnect(); // Choisir wifiConnect() pour une connection Wifi avec les identifiants Wifi
-  wifiConnectWPS(); // Choisir wifiConnectWPS() pour une connection Wifi en WPS
+  /*
+   * Choisr SOIT wifiConnect() SOIT wifiConnectWPS();
+   */
+  //wifiConnect(); // Choisir wifiConnect() pour une connexion Wifi avec les identifiants Wifi dans le code
+  wifiConnectWPS(); // Choisir wifiConnectWPS() pour une connexion Wifi en WPS
 }
  
 void loop() {
-
-  if ((WiFi.status() == WL_CONNECTED)) { // Vérification du status Wifi
-    
+  bool errorHttp = false; 
+  if ((WiFi.status() == WL_CONNECTED)) { // Vérification du status Wifi  
     HTTPClient http;
- 
     http.begin(endpoint + key); // URL vers l'API
     int httpCode = http.GET(); 
- 
     if (httpCode > 0) { // Vérification du code HTTP retourné
         String payload = http.getString();
-        Serial.println(httpCode);
-        Serial.println(payload);
-        
+        Serial.println("Code http : " + (String)httpCode);
+        Serial.println(payload);       
         displayWeather(payload);
-    }
-    else {
+    } else {
       Heltec.display -> clear();
       Heltec.display->setTextAlignment(TEXT_ALIGN_CENTER);
       Heltec.display->setFont(ArialMT_Plain_16);
-      Heltec.display -> drawString(64,32,"Erreur réseau");
+      Heltec.display -> drawString(64,4,"Reconnexion");
+      Heltec.display -> drawString(64,20,"Au service météo");
+      Heltec.display -> drawString(64,36,"Patientez...");
       Heltec.display -> display();
+      cycle=0; // On ne repasse pas en veille tant que nous sommes en erreur de requête http
       Serial.println("Erreur requete HTTP");
+      errorHttp = true;        
     }
-
     http.end(); // libération des ressources
-    delay(29000);
+    if (!errorHttp) { //Si la connexion est en erreur on n'effectue pas le calcul de la moyenne batterie pour ne pas perdre 28 secondes
+      averageBattery=averagePower(); // Calcul du niveau de batterie en faisant une moyenne sur 28 secondes
+    }
+  } else {
+    cycle=0; // on ne passe pas en veille tant que la connexion wifi n'est pas établie
   }
-  delay(1000);
-
+  delay(2000);
   cycle++;
   Serial.println("Cycle = " + (String)cycle);
   if (cycle >= 3) { //Déclenchement de la veille au bout de la 3ème boucle
     cycle=0;
     lightSleep();
-  }
-  
+  } 
 }
